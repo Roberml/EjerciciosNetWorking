d1259cdd73a1bb51e1f4f00a575cc59e38edbedf
Roberml <66039985+Roberml@users.noreply.github.com>
2021-06-09 19:00:20 +0200

    Add files via upload

---
+++ Ej1ServiciosNet/Cliente/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente
+{
+    public partial class Form1 : Form
+    {
+        public string IP = "127.0.0.1";
+        public int port = 31416;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            string userMsg = b.Text;
+
+            IPEndPoint ie = new IPEndPoint(IPAddress.Parse(IP), port);
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    s.Connect(ie);
+                    using (NetworkStream ns = new NetworkStream(s))
+                    using (StreamReader sr = new StreamReader(ns))
+                    using (StreamWriter sw = new StreamWriter(ns))
+                    {
+                        sw.WriteLine(userMsg);
+                        sw.Flush();
+                        label1.Text = sr.ReadLine();
+                    }
+                }
+                catch (Exception)
+                {
+
+
+                }
+            }
+
+
+
+            if (b.Text == "APAGAR")
+            {
+                this.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            DialogResult res = f.ShowDialog();
+
+            switch (res)
+            {
+                
+                case DialogResult.OK:
+                    this.IP = f.textBox1.Text;
+                    this.port = Convert.ToInt32(f.textBox2.Text);
+                    break;
+              
+              
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Ej1ServiciosNet/Ej1ServiciosNet/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ej1ServiciosNet
+{
+    class Program
+    {
+
+        static string msg = "";
+        public static int port = 31416;
+        static bool puertoOcupado = true;
+
+        static void funcionHilo(object socket)
+        {
+            using (Socket sCliente = (Socket)socket)
+            {
+                IPEndPoint ipCliente = (IPEndPoint)sCliente.RemoteEndPoint;
+                Console.WriteLine("Estás conected");
+
+                using (NetworkStream ns = new NetworkStream(sCliente))
+                using (StreamReader sr = new StreamReader(ns))
+                using (StreamWriter sw = new StreamWriter(ns))
+
+                {
+                    msg = sr.ReadLine();
+
+
+
+                    try
+                    {
+
+
+                        if (msg != null)
+                        {
+
+                            switch (msg)
+                            {
+
+
+                                case "HORA":
+                                    Console.WriteLine(msg);
+                                    sw.WriteLine(DateTime.Now.ToString("T"));
+                                    break;
+
+
+                                case "FECHA":
+                                    sw.WriteLine(DateTime.Now.ToString("D"));
+
+                                    break;
+
+
+
+                                case "TODO":
+                                    sw.WriteLine(DateTime.Now);
+
+                                    break;
+
+
+                                case "APAGAR":
+
+                                    break;
+
+
+                            }
+                        }
+                    }
+
+
+                    catch (Exception e) when (e is NullReferenceException || e is IOException)
+                    {
+
+
+                    }
+
+
+                    Console.WriteLine("Client disconnected.\nConnection closed");
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                do
+                {
+                    try
+                    {
+
+                        IPEndPoint ie = new IPEndPoint(IPAddress.Any, port);
+                        s.Bind(ie);
+
+                    }
+                    catch (SocketException e)
+                    {
+                        port++;
+
+                    }
+
+
+
+
+                } while (!puertoOcupado);
+
+
+                s.Listen(10);
+
+
+                while (msg != "APAGAR")
+                {
+                    Socket sCliente = s.Accept();
+                    Thread hilo = new Thread(funcionHilo);
+                    hilo.Start(sCliente);
+                }
+
+
+
+
+                Console.WriteLine("Estás conected");
+
+
+
+            }
+        }
+    }
+}
