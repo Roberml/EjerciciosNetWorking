8faab0637d5e81c738351c8a86b0989dea8639f3
Roberml <66039985+Roberml@users.noreply.github.com>
2021-06-09 14:16:19 +0200

    Add files via upload

---
+++ Ej3ServiciosNet/Ej3ServiciosNet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3ServiciosNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sala s1 = new Sala();
+            s1.iniciaServicioChat();
+            Console.ReadKey();
+
+            }
+        }
+    
+}
